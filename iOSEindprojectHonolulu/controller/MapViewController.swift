e7299e212bae27c95c12ff96d1518db5c288500e
David Gunzburg <david.gunzburg@gmal.com>
2020-01-29 12:13:15 +0100

    Segmented controller to change Map type
    Initial Map Region not yet working

---
+++ iOSEindprojectHonolulu/controller/MapViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MapViewController.swift
+//  iOSEindprojectHonolulu
+//
+//  Created by mobapp02 on 29/01/2020.
+//  Copyright © 2020 mobapp02. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        func mapView(_ mapView: MKMapView) {
+            let visibleRegion = MKCoordinateRegion.init(center: CLLocationCoordinate2DMake(21.3069, 157.8583), latitudinalMeters: 2000, longitudinalMeters: 2000)
+            mapView.region = visibleRegion
+        }
+    }
+    
+    @IBAction func changeMapType(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0: mapView.mapType = .standard
+        case 1: mapView.mapType = .satellite
+        default:print()
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
---
+++ iOSEindprojectHonolulu/controller/TableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TableViewController.swift
+//  iOSEindprojectHonolulu
+//
+//  Created by mobapp02 on 29/01/2020.
+//  Copyright © 2020 mobapp02. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
